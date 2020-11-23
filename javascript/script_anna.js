abecc5840f1909715885396d923996de725f8a56
Anna Roos <anna.flytstrom@gmail.com>
2020-11-23 14:24:05 +0100

    deleted unnecessary file

--- javascript/script_anna.js
+++ javascript/script_anna.js
@@ -5,32 +5,63 @@ const astronautNames = document.querySelector('.r3_list_of_names');
 let latitude = document.querySelector('.lat_num');
 let longitude = document.querySelector('.long_num');
 
-const astronautData = fetch(astroUrl)
-    .then(response => response.json())
-    .then(data => {
-        let astroNo = data.number;
-        let astroNames = data.people;
+
+const apiRequest = (url, method) => {
+    return fetch(url, {
+        method: method
+      })
+        .then(response => {
+          if (response.status >= 200 && response.status < 300) {
+            return response.json();
+          } else {
+            return response.json().then(errData => {
+              console.log(errData);
+              throw new Error('Server is not responding.');
+            });
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          throw new Error('Network error');
+        });
+}
+
+const getAstronautData = async () => {
+    try{
+        const response = await apiRequest(astroUrl, 'GET');
+        let astroNo = response.number;
+        let astroNames = response.people;
         astronautsInSpace.innerText = astroNo;
         for(person of astroNames) {
             let newName = document.createElement('li');
             newName.innerText = person.name;
             astronautNames.appendChild(newName);
         }
-})
+    } catch(error){
+        console.log(error);
+    }
 
-const getIssData = async function () {
-    const response = await fetch(issUrl);
-    const data = await response.json();
-    let newLatitude = data.iss_position.latitude;
-    let newLongitude = data.iss_position.longitude;
-    latitude.innerText = newLatitude;
-    longitude.innerText = newLongitude;
+}
+
+const getIssData = async () => {
+    try {
+        const response = await apiRequest(issUrl, 'GET');
+        const data = response;
+        let newLatitude = data.iss_position.latitude;
+        let newLongitude = data.iss_position.longitude;
+        latitude.innerText = newLatitude;
+        longitude.innerText = newLongitude;
+    } catch(error) {
+        console.log(error);
+    }
+
+
 }
 
 const issDataIntervals = (n) => {
     setInterval(getIssData, n);
 }
 
+getAstronautData();
 getIssData();
 issDataIntervals(2000);
-
