154996d429d5b22631004567398fdedbf927f048
wikidbrit <pflemingonline@gmail.com>
2020-12-01 09:15:20 +0100

    Corrections to readme, corrections to chatbot styling, various corrections to HTML position of items, JS fixes for date catch

--- javascript/script_adam.js
+++ javascript/script_adam.js
@@ -4,6 +4,7 @@
 
 const quoteUrl = 'https://type.fit/api/quotes'
 const dailyQuote = document.querySelector('.r16_quotetext')
+const dailyQuoteAuthor = document.querySelector('.r17_bodytext')
 
 const getQuoteData = async () => {
     try {
@@ -12,6 +13,7 @@ const getQuoteData = async () => {
         console.log(data);
         let num = Math.floor(Math.random() * 10)
         dailyQuote.innerText = JSON.stringify(data[num].text)
+        dailyQuoteAuthor.innerText = JSON.stringify(data[num].author)
     } catch(error) {
         console.log(error);
     }
